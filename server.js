cd48ace4e6b2ba52d1acd4b24248ffbe00a168b7
sunil-kumar-26 <sunilworld1234ss@gmail.com>
2026-02-04 18:08:04 +0530

    fix:port added into env

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/connectDB");
 
-const PORT = 4000;
 (async () => {
   try {
     await connectDB(process.env.MONGODB_URL);
-    app.listen(PORT, () => {
-      console.log(`server is runing on http://localhost:${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`server is runing on http://localhost:${process.env.PORT}`);
     });
   } catch (err) {
     console.log("Server crasehd Please see ERROR", err);
